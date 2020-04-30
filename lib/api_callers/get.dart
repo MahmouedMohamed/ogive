0ea9daf1e20773bc05b9d56aa31aa7a16f0f5168
mahmoued <mahmouedmartin222@yahoo.com>
2020-04-30 16:42:33 +0200

    Updated api callers to work with firebase

--- lib/api_callers/get.dart
+++ lib/api_callers/get.dart
@@ -1,105 +1,81 @@
 import 'dart:async';
 import 'dart:convert';
-
-import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:ogive/custom_widgets/marker_icon.dart';
 import 'package:ogive/models/user.dart';
 import 'package:ogive/models/weather.dart';
 
-final String URL = 'http://192.168.1.103:8000/api/';
-
+final fb = FirebaseDatabase.instance;
 Future<List<Marker>> getMarkers() async {
-  List data;
-  MarkerIcon markerOption = new MarkerIcon();
+  final ref = fb.reference();
   List<Marker> markers = new List<Marker>();
-  var response = await http
-      .get(Uri.encodeFull(URL+'Markers'), headers: {"Accpet": "application/json"});
-  var convertDataToJson = jsonDecode(response.body);
-  data = convertDataToJson['markers'];
-  for (int i = 0; i < data.length; i++) {
-    MarkerId markerId = new MarkerId(data[i]['id'].toString());
-    double latitude = double.parse(data[i]['Latitude'].toString());
-    double longitude = double.parse(data[i]['Longitude'].toString());
-    markers.add(Marker(
-      markerId: markerId,
-      position: LatLng(latitude, longitude),
-      icon: markerOption.getIcon(),
-      infoWindow:
-          InfoWindow(title: markerId.toString(), snippet: markerId.toString()),
-    ));
-  }
+  MarkerIcon markerOption = new MarkerIcon();
+  await ref.child('Markers').orderByKey().once().then((value) {
+    Map<dynamic, dynamic> map = value.value;
+    for (int i = 0; i < map.keys.length; i++) {
+      MarkerId markerId =
+          new MarkerId(map.keys.toList().elementAt(i).toString());
+      double latitude =
+          double.parse(map.values.toList().elementAt(i)['Latitude'].toString());
+      double longitude = double.parse(
+          map.values.toList().elementAt(i)['Longitude'].toString());
+      markers.add(Marker(
+        markerId: markerId,
+        position: LatLng(latitude, longitude),
+        icon: markerOption.getIcon(),
+        infoWindow: InfoWindow(
+            title: markerId.toString(), snippet: markerId.toString()),
+      ));
+    }
+  });
+  print('thing $markers');
   return markers;
 }
 
-Future<Weather> getWeatherCondition(latitude,longitude) async{
-  final String weatherURL = 'http://api.openweathermap.org/data/2.5/weather?lat='
-  +latitude.toString()+"&lon="+longitude.toString()+"&appid="+'eeb0971ab03a92f6318c8907c2c13e20';
+Future<Weather> getWeatherCondition(latitude, longitude) async {
+  final String weatherURL =
+      'http://api.openweathermap.org/data/2.5/weather?lat=' +
+          latitude.toString() +
+          "&lon=" +
+          longitude.toString() +
+          "&appid=" +
+          'eeb0971ab03a92f6318c8907c2c13e20';
   var headers = {
     "Accpet": "application/json",
     "Content-Type": "application/json"
   };
   Weather weather;
-  var response = await http
-      .get(Uri.encodeFull(weatherURL), headers: headers);
+  var response = await http.get(Uri.encodeFull(weatherURL), headers: headers);
   var convertDataToJson = jsonDecode(response.body);
   weather = new Weather(
     latitude,
     longitude,
     convertDataToJson['weather'][0]['main'].toString().toLowerCase(),
-    double.parse(convertDataToJson['main']['temp'].toString())-273.15,
-    double.parse(convertDataToJson['main']['temp_min'].toString())-273.15,
-    double.parse(convertDataToJson['main']['temp_max'].toString())-273.15,
+    double.parse(convertDataToJson['main']['temp'].toString()) - 273.15,
+    double.parse(convertDataToJson['main']['temp_min'].toString()) - 273.15,
+    double.parse(convertDataToJson['main']['temp_max'].toString()) - 273.15,
     double.parse(convertDataToJson['main']['pressure'].toString()),
-    double.parse(convertDataToJson['main']['humidity'].toString())/100,
+    double.parse(convertDataToJson['main']['humidity'].toString()) / 100,
     double.parse(convertDataToJson['wind']['speed'].toString()),
   );
   return weather;
 }
-Future<Map<String,dynamic>> getUser(email,password) async {
-  var data;
-//  print('thing login NOWW $email, $password}');
-  var body = {'email': email, 'password': password};
-  var response = await http
-      .post(Uri.encodeFull(URL+'login'), headers: {"Accpet": "application/json"},body: body);
-//  print('thing login NOWWWWW');
-  int responseStatus = response.statusCode;
-  if(response.statusCode != 200){
-    return null;
-  }
-  else{
-    var convertDataToJson = jsonDecode(response.body);
-//    print('thing login $convertDataToJson');
-//    print('thing ${convertDataToJson['user']['email_verified_at'] == null}');
-    User user = new User(
-      convertDataToJson['user']['id'].toString(),
-      convertDataToJson['user']['name'],
-      convertDataToJson['user']['user_name'],
-      convertDataToJson['user']['email'],
-      convertDataToJson['user']['email_verified_at'] == null ? null : DateTime.parse(convertDataToJson['user']['email_verified_at']),
-      DateTime.parse(convertDataToJson['user']['created_at']),
-      DateTime.parse(convertDataToJson['user']['updated_at']),
+
+Future<User> getUser(userId) async {
+  final ref = fb.reference();
+  User user;
+  await ref.child('Users').child('$userId').once().then((value) {
+    Map<dynamic, dynamic> map = value.value;
+    print('thing ${map['name']}');
+    user = new User(
+      userId,
+      map['name'],
+      map['email'],
+      DateTime.parse(map['created_at']),
     );
-    Map<String,dynamic> map = {"oauthToken": convertDataToJson['token'],"user" : user};
-//    data = convertDataToJson['token'];
-//    print('thing responseStatus ${convertDataToJson['user']['id']}');
-//    DateTime(year)
-//    print('thing DateTime ${DateTime.parse(convertDataToJson['user']['created_at'])}');
-    print('thing  $map');
-    return map;
-  }
-//  for (int i = 0; i < data.length; i++) {
-//    MarkerId markerId = new MarkerId(data[i]['id'].toString());
-//    double latitude = double.parse(data[i]['Latitude'].toString());
-//    double longitude = double.parse(data[i]['Longitude'].toString());
-//    markers.add(Marker(
-//      markerId: markerId,
-//      position: LatLng(latitude, longitude),
-//      icon: markerOption.getIcon(),
-//      infoWindow:
-//      InfoWindow(title: markerId.toString(), snippet: markerId.toString()),
-//    ));
-//  }
-//  return markers;
+    print('thing good ${user.getName()}');
+  });
+  return user;
 }
