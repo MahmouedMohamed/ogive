62c21649a7ec05fab753109ea040c432e0ca2405
mahmoued <mahmouedmartin222@yahoo.com>
2020-04-30 16:41:54 +0200

    Added text field widget

--- lib/custom_widgets/text_field.dart
+++ lib/custom_widgets/text_field.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
 
-Widget textField(labelText, selectedIcon, selectedColor, status, type) {
+Widget textField(controller,labelText, selectedIcon, selectedColor, status, type,hint,errorMessage) {
   return Padding(
-    padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+    padding: EdgeInsets.only(left: 10, right: 10, bottom: 10,top: 20),
     child: TextField(
+      controller: controller,
       decoration: InputDecoration(
-        labelText: labelText,
-        labelStyle: TextStyle(color: selectedColor),
-        icon: Icon(
-          selectedIcon,
-          size: 30,
-          color: selectedColor,
-        ),
-        enabledBorder: UnderlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(30)),
-          borderSide: BorderSide(color: selectedColor),
-        ),
-        border: OutlineInputBorder(
-            borderSide: BorderSide(color: selectedColor),
-            borderRadius: BorderRadius.all(Radius.circular(30))),
-        focusColor: selectedColor,
+          labelText: labelText,
+          labelStyle: TextStyle(color: Colors.white),
+          icon: Icon(
+            selectedIcon,
+            size: 30,
+            color: selectedColor,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            borderSide: BorderSide(color: Color.fromRGBO(255, 216, 3, 1)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(width: 1, color: Color.fromRGBO(255, 216, 3, 1)),
+            gapPadding: 4,
+          ),
+        hintText: hint,
+        errorText: errorMessage,
+//        errorBorder: OutlineInputBorder()
       ),
       obscureText: status,
       keyboardType: type,
