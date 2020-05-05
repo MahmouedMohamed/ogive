f66c4e6927b4207a3f87e397c75bbe01abd2a96f
mahmoued <mahmouedmartin222@yahoo.com>
2020-05-05 02:02:03 +0200

    Added post marker feature

--- lib/screens/feed_me_intro.dart
+++ lib/screens/feed_me_intro.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ogive/api_callers/post.dart';
+import 'package:ogive/models/user_location.dart';
+import 'package:toast/toast.dart';
+
+import '../session_manager.dart';
 
 class FeedMeIntro extends StatelessWidget {
   @override
@@ -8,7 +13,17 @@ class FeedMeIntro extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          RaisedButton(onPressed: (){},
+          RaisedButton(onPressed: () async {
+            UserLocation userLocation= new UserLocation();
+            SessionManager sessionManager =  new SessionManager();
+            if(userLocation.getLatLng()==null)
+              await userLocation.getUserLocation();
+            Map<String,dynamic> map = await createMarker(userLocation.getLatLng().latitude,userLocation.getLatLng().longitude,sessionManager.getUser().getID(),1);
+            Toast.show(
+              '${map.values.elementAt(0)}',
+              context
+            );
+          },
             child: Text('add Marker'),
           ),
           RaisedButton(onPressed: (){
